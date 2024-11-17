2daac17c2a60b73a7f25b1bd65bdc0d8607f6125
taNMay9120 <tnmsinha65@gmail.com>
2024-11-17 18:16:07 +0530

    pic changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,17 +5,17 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I m Ada</h1>
+        <h1 className={styles.title}>Hi, I m Tanmay</h1>
         <p className={styles.description}>
-          I am a full-stack developer with almost a  year of experience using React and
-          NodeJS.
+          I am a full-stack developer with almost a year of experience using
+          React and NodeJS.
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:tnmsinha65@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/pic2.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
