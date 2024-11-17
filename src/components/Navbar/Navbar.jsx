d3c8feb96fc5cbba81335671aab4cc3287902752
taNMay9120 <tnmsinha65@gmail.com>
2024-11-17 19:18:21 +0530

    fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./navbar.module.css";
+import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
