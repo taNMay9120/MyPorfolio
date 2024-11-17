2d4249d76aca4d3dd39a1faf0721e09b628305f8
taNMay9120 <tnmsinha65@gmail.com>
2024-11-17 18:04:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import styles from "./app.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Contact } from "./components/Contact/Contact";
+import { Projects } from "./components/Projects/Projects";
+
+function App() {
+  if (styles === null || styles === undefined) {
+    throw new Error("styles is null or undefined");
+  }
+
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero/>
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I m Ada</h1>
+        <p className={styles.description}>
+          I am a full-stack developer with almost a  year of experience using React and
+          NodeJS.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
